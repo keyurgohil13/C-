82ed2ba28af6e93616418a56748800318997b1ba
keyurgohil13 <gohilkeyur13@gmail.com>
2025-02-03 18:28:38 +0530

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int num = 100;
+    float d_num = 20.3;
+    char special_char = '@';
+    bool yes = true;
+
+    float ans;
+
+    ans = num + d_num;
+ 
+    cout << ans;
+
+    return 0;
+
+}
---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a;
+    int b;
+    int c;
+
+    cout << "Enter Value of a-";
+    cin >> a;
+
+    cout << "Enter Value of b-";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Ans is " << c << endl << endl << "Hello" ;
+
+    return 0;
+
+
+}
